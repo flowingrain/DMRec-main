eecf3256aa1b2bf4de8fb3063e86c3dbe772ac59
ShLi <shlip@foxmail.com>
2025-11-11 16:58:45 +0800

    DMRec原始实现

---
+++ encoder/config/beta.md
@@ -0,0 +1,9 @@
+`beta` is configured differently across various base models and datasets; therefore, we provide the detailed settings here for reference. Please note that the results of a single experiment may differ from those reported in the paper, as the reported results are the average over multiple runs.
+
+- Mult-VAE:
+
+| **`beta`** | **Amazon-Book** | **Yelp** | **Steam** |
+|----------------|-----------------|----------|-----------|
+| GODM           |0.4              | 0.8      | 1.0       |
+| CPDM           |0.2              | 0.9      | 0.6       |
+| MDDM           |0.6              | 0.4      | 0.5       |
---
+++ encoder/models/general_cf/mult_vae_mddm.py
@@ -0,0 +1,140 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+from config.configurator import configs
+from models.loss_utils import cal_bpr_loss, reg_params
+from models.base_model import BaseModel
+import numpy as np
+
+init = nn.init.xavier_uniform_
+uniformInit = nn.init.uniform
+
+
+class mult_vae_MDDM(BaseModel):
+    def __init__(self, data_handler):
+        super(mult_vae_MDDM, self).__init__(data_handler)
+
+        self.beta = self.hyper_config['beta']
+
+        self.data_handler = data_handler
+
+        # According to the original paper, the default structure is [item_num, 600, 200, 600, item_num]
+        self.p_dims = [200, 600, self.item_num]
+        self.q_dims = [self.item_num, 600, 200]
+
+        # Compute the mean and variance in parallel.
+        temp_q_dims = self.q_dims[:-1] + [self.q_dims[-1] * 2]
+
+        self.q_layers = nn.ModuleList(
+            [nn.Linear(d_in, d_out) for d_in, d_out in zip(temp_q_dims[:-1], temp_q_dims[1:])]
+        )
+
+        # Load the representations of user (or item) profiles.
+        self.usrprf_embeds = torch.tensor(configs['usrprf_embeds']).float().cuda()
+        self.itmprf_embeds = torch.tensor(configs['itmprf_embeds']).float().cuda()  # [item_num, 1536]
+
+        self.mlp = nn.Sequential(
+            nn.Linear(self.itmprf_embeds.shape[1], 600),
+            nn.Tanh(),
+            nn.Linear(600, 400)
+        )
+
+        self.p_layers = nn.ModuleList(
+            [nn.Linear(d_in, d_out) for d_in, d_out in zip(self.p_dims[:-1], self.p_dims[1:])]
+        )
+
+        self.drop = nn.Dropout(self.hyper_config['dropout'])
+
+        self.final_embeds = None
+        self.is_training = False
+
+    def encode(self, x, user_emb):
+        h = self.drop(x)
+        # [batch, item_num] * [item_num, dim] = [batch, dim]
+        hidden = torch.matmul(h, self.itmprf_embeds) + user_emb
+        hidden = self.mlp(hidden)
+
+        mu_llm = hidden[:, :200]
+        logvar_llm = hidden[:, 200:]
+
+        for i, layer in enumerate(self.q_layers):
+            h = layer(h)
+            if i != len(self.q_layers) - 1:
+                h = torch.tanh(h)
+            else:
+                mu = h[:, :self.q_dims[-1]]
+                logvar = h[:, self.q_dims[-1]:]
+
+        return mu, mu_llm, logvar, logvar_llm
+
+    def decode(self, z):
+        h = z
+        for i, layer in enumerate(self.p_layers):
+            h = layer(h)
+            if i != len(self.p_layers) - 1:
+                h = torch.tanh(h)
+        return h
+
+    def reparameterize(self, mu, logvar):
+        if self.is_training:
+            std = torch.exp(0.5 * logvar)
+            eps = torch.randn_like(std)
+            z = eps.mul(std) + mu
+        else:
+            z = mu
+        return z
+
+    def cal_loss(self, user, batch_data):
+        self.is_training = True
+
+        user_emb = self.usrprf_embeds[user]
+
+        mu_src, mu_llm, logvar_src, logvar_llm = self.encode(batch_data, user_emb)
+
+        # There are two processing strategies:
+        # (1) performing downstream tasks after addition, and
+        # (2) directly conducting downstream tasks.
+        # In practice, we found that these two strategies have their own advantages on different datasets.
+        # Therefore, we adopt a unified design that performs reconstruction and matching after addition.
+        mu = mu_src + mu_llm
+        logvar = logvar_src + logvar_llm
+
+        # Perform downstream tasks directly
+        # mu = mu_src
+        # logvar = logvar_src
+
+        z = self.reparameterize(mu, logvar)
+        recon_x = self.decode(z)
+
+        BCE = - torch.mean(torch.sum(F.log_softmax(recon_x, 1) * batch_data, -1))
+
+        KLD = - 0.5 * torch.mean(torch.sum(1 + logvar - mu.pow(2) - logvar.exp(), dim=1))
+        KLD_llm = - 0.5 * torch.mean(torch.sum(1 + torch.log(logvar.exp()/(logvar_llm.exp() + 10e-8) + 10e-8) -
+                                           (mu - mu_llm).pow(2)/(logvar_llm.exp() + 10e-8) - logvar.exp()/(logvar_llm.exp() + 10e-8), dim=1))
+
+        KLD = self.beta * KLD + (1 - self.beta) * KLD_llm
+
+        loss = BCE + KLD
+        losses = {'rec_loss': BCE, 'reg_loss': KLD}
+        return loss, losses
+
+    def full_predict(self, batch_data):
+        self.is_training = False
+        pck_users, train_mask = batch_data
+        pck_users = pck_users.long()
+
+        batch_data = self.data_handler.train_data[pck_users.cpu()]
+        data = torch.FloatTensor(batch_data.toarray()).to(configs['device'])
+        user_emb = self.usrprf_embeds[pck_users]
+
+        mu, mu_llm, logvar, logvar_llm = self.encode(data, user_emb)
+
+        # If the downstream tasks are performed directly, this section should be commented out.
+        mu = mu + mu_llm
+        logvar = logvar + logvar_llm
+
+        z = self.reparameterize(mu, logvar)
+        recon_x = self.decode(z)
+
+        full_preds = self._mask_predict(recon_x, train_mask)
+        return full_preds
